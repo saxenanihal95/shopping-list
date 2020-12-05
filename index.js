de79e386dbdefa7e61990df54b9dae6442099456
saxenanihal95 <saxenanihal95@gmail.com>
2020-12-05 10:22:54 +0530

    save user to localstorage

--- index.js
+++ index.js
@@ -9,5 +9,23 @@ function handleInput(e) {
 
 // submit name to redirect to shopping list
 function submitName() {
-    window.location.href = "shopping-list.html"
+  // get list of users from localStorage
+  const users = JSON.parse(localStorage.getItem("users"));
+
+  // if users key is present in localStore create key and add user
+  if (!users) {
+    const users = [{ name: inputValue, list: [] }];
+    localStorage.setItem("users", JSON.stringify(users));
+  } else {
+    const existingUser = users.find(({ name }) => name === inputValue);
+    console.log(existingUser, users);
+    if (!existingUser) {
+        users.push({ name: inputValue, list: [] });
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+  }
+
+  nameInput.value=""
+
+  window.location.href = "shopping-list.html";
 }
