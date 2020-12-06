1f9e3cd637585cd3fe315b040aa16c24837d2c69
saxenanihal95 <saxenanihal95@gmail.com>
2020-12-06 19:26:03 +0530

    Add and remove Item

--- shopping-list.js
+++ shopping-list.js
@@ -14,29 +14,88 @@ function getLoggedInUser() {
 function getShoppingList() {
   const loggedInUser = getLoggedInUser();
   if (!loggedInUser) redirectToHome();
-  return JSON.parse(localStorage.getItem(loggedInUser));
+  return JSON.parse(localStorage.getItem(loggedInUser)) || [];
 }
 
 function addToShoppingList(item) {
-  displayItem(item)
   const loggedInUser = getLoggedInUser();
-  const existingList = getShoppingList() || [];
-  existingList.push(item);
-  localStorage.setItem(loggedInUser, JSON.stringify(existingList))
+  const existingList = getShoppingList();
+  const id = existingList.length + 1;
+  displayItem(item, id);
+  existingList.push({ id, item });
+  localStorage.setItem(loggedInUser, JSON.stringify(existingList));
 }
 
-function displayItem(value) {
-  const li = document.createElement("li");
-  const inputValue = value;
-  const textNode = document.createTextNode(inputValue);
-  li.appendChild(textNode);
-  document.getElementById("shopping-list").appendChild(li);
+function getIndexById(arr, id) {
+  return arr.findIndex((item) => item.id === id);
+}
+
+function saveItem(value, id) {
+  const loggedInUser = getLoggedInUser();
+  const existingList = getShoppingList();
+  const updatedList = existingList.map((item) => {
+    if (item.id === id) return { ...item, value };
+    return item;
+  });
+  localStorage.setItem(loggedInUser, JSON.stringify(updatedList));
+}
+
+function deleteItem(id) {
+  const loggedInUser = getLoggedInUser();
+  const existingList = getShoppingList();
+  const index = getIndexById(existingList, id);
+  if (index > -1) {
+    existingList.splice(index, 1);
+  }
+  localStorage.setItem(loggedInUser, JSON.stringify(existingList));
+}
+
+function displayItem(value, id) {
+  const div = document.createElement("div");
+  div.ondblclick = () => {
+    input.disabled = false;
+  };
+  div.id = `input-${id}`;
+
+  const input = document.createElement("input");
+  input.value = value;
+  input.disabled = true;
+  input.oninput = handleInput;
+  let inputValue = value;
+  // handleChange for name input
+  function handleInput(e) {
+    inputValue = e.target.value;
+  }
+
+  const editButton = document.createElement("button");
+  let isEditable = false;
+  editButton.textContent = isEditable ? "Done" : "Edit";
+  editButton.onclick = () => {
+    if (isEditable) {
+      saveItem(inputValue, id);
+    }
+    isEditable = !isEditable;
+    input.disabled = !isEditable;
+    editButton.textContent = isEditable ? "Done" : "Edit";
+  };
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.onclick = () => {
+    document.getElementById(`input-${id}`).remove();
+    deleteItem(id);
+  }
+
+  div.appendChild(input);
+  div.appendChild(editButton);
+  div.appendChild(deleteButton);
+  document.getElementById("shopping-list").appendChild(div);
 }
 
 function onLoad() {
   const shoppingList = getShoppingList();
   if (shoppingList) {
-    shoppingList.map((listItem) => displayItem(listItem));
+    shoppingList.map((listItem, i) => displayItem(listItem.value, listItem.id));
   }
 }
 
